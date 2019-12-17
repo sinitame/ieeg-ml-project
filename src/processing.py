82d9b236a8791687debcda75951e6bb746f97aa9
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2019-12-17 18:49:17 +0100

    Removing processing.py

--- src/processing.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def calculate_signal_power(signal, start, end):
-    """
-    This function calculate the energy of a signal between a range
-    """
-    return np.sum(np.power(signal[start:end],2))
